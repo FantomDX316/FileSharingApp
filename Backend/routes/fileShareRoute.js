d837b848755c0f8e30817c991df0c91c983b67e6
saksham <phantomdx316@gmail.com>
2023-04-08 20:24:40 +0530

    added timeout to remove files

--- Backend/routes/fileShareRoute.js
+++ Backend/routes/fileShareRoute.js
@@ -3,6 +3,7 @@ const path = require("path");
 const router = express.Router();
 const multer = require("multer");
 const { body, validationResult } = require("express-validator");
+// const cron = require("node-cron");
 const fs = require("fs");
 
 //storage option for multer
@@ -24,6 +25,9 @@ const fileShareModel = require("../model/fileShareModel.js");
 
 
 // Route 1 to upload the file to the server
+router.get("/",(req,res)=>{
+    res.render("download");
+});
 router.post("/uploadfile", (req, res) => {
     try {
         upload(req, res, async function (err) {
@@ -31,14 +35,14 @@ router.post("/uploadfile", (req, res) => {
                 res.status(500).json({ "success": false, "error": "file size should be less than 5mb" });
             } else {
                 // console.log(req.file)
-
+                console.log(__dirname);
                 const code = parseInt(100000 + Math.random() * 900000);
                 const fileShare = new fileShareModel({
                     fileName: req.file.filename,
                     filePath: req.file.path,
                     fileCode: code
                 });
-                fileShare.save().then().catch((error) => { res.status(500).json({ "success": false, "error": error.message }) });
+                fileShare.save().then(file => console.log(file)).catch((error) => { res.status(500).json({ "success": false, "error": error.message }) });
 
                 res.status(200).json({ "success": true, "fileCode": code });
 
@@ -52,23 +56,32 @@ router.post("/uploadfile", (req, res) => {
 
 //Route 2 to recieve the file from the server
 
-router.post("/recievefile", [body("fileCode", "code should be of 6 characters").isLength({ min: 6, max: 6 })], async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(500).json({ "success": false, errors });
-        };
+router.get("/recievefile",(req, res) => {
+    // try {
+    //     const errors = validationResult(req);
+    //     if (!errors.isEmpty()) {
+    //         return res.status(500).json({ "success": false, errors });
+    //     };
 
-        const { fileCode } = req.body;
-        const file = await fileShareModel.findOne({ fileCode });
-        if (!file) {
-            return res.status(500).json({ "success": false });
-        } else {
-            console.log(file)
-            res.json({ "success": true });
-        }
+    //     const { fileCode } = req.body;
+    //     const file = await fileShareModel.findOne({ fileCode });
+    //     if (!file) {
+    //         return res.status(500).json({ "success": false, "error": "file has been expired" });
+    //     } else {
+            res.status(200).render("download",{data:{
+                expires:false,
+                fileName:"hello",
+                fileSize:"2kb"
+            }})
+            // res.download(file.filePath, file.fileName);
+            // setTimeout(() => {
+            //     file.deleteOne({ fileCode: file.fileCode });
+            //     fs.unlinkSync(file.filePath);
+            //     console.log("files deleted");
+            // }, 1000 * 600);
+        // }
 
-    } catch (error) { res.status(500).json({ "success": false }) }
+    // } catch (error) { res.status(500).json({ "success": false }) }
 });
 
 
