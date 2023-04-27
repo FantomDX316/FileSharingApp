7aa6c0b7a990c400799bc5088477261b7c1dc85d
saksham <phantomdx316@gmail.com>
2023-04-27 22:34:35 +0530

    checked the file input

--- Frontend/src/components/Home/Home.js
+++ Frontend/src/components/Home/Home.js
@@ -5,14 +5,25 @@ import axios from "axios";
 
 
 const Home = ()=>{
+
+    const [data,setData] = useState("");
+
+    const inputHandler = (e)=>{
+        setData(e.target.files[0]);
+    }
+
+    const uploadHandler = (e)=>{
+        e.preventDefault();
+        axios.post("http://localhost:5000",data);
+    }
     return(
     <>
     
         <div className="container d-flex justify-content-center">
             <div className="card d-flex justify-content-center align-items-center  ">
                     <h1>Upload File</h1>
-                    <form >
-                        <input type="file"/>
+                    <form onSubmit={uploadHandler}>
+                        <input type="file" onChange={inputHandler}/>
                         <button type="submit"  className="btn btn-primary">Upload File</button>
                     </form>
             </div>
