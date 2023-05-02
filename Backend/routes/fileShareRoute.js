a0f18cb58415ea2f26660fe2a7afe8dfbec17d65
saksham <phantomdx316@gmail.com>
2023-05-02 22:55:28 +0530

    added file upload route and tested

--- Backend/routes/fileShareRoute.js
+++ Backend/routes/fileShareRoute.js
@@ -1,2 +1,35 @@
-const express  = require("express");
+const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+
+//storage option for multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/uploads")
+    },
+    filename: (req, file, cb) => {
+        cb(null,`${Date.now()}-${file.originalname}`);
+    }
+});
+
+//calling multer constructor function
+const maxSize = 1000 * 1024 * 5;
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: maxSize
+    }
+})
+
+
+//file route
+router.post("/fileUpload", upload.single("data"), (req, res) => {
+    try {
+        console.log(req.file);
+        res.send("success")
+
+    } catch (error) { console.log(error) }
+})
+module.exports = router;
+
+
