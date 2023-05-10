792df9fba3d67e72f3ed2bf4a23f19d1e525062d
saksham <phantomdx316@gmail.com>
2023-05-10 23:58:15 +0530

    added OTP displaying logic

--- Frontend/src/components/Alert/Alert.js
+++ Frontend/src/components/Alert/Alert.js
@@ -6,7 +6,7 @@ const Alert = (props) => {
     const {alertContent} = context;
     return (
         <>
-            <div style={{position:"absolute",left:"50%",top:"10%",translate:"-50% 0",zIndex:"3",backgroundColor:"red",color:"white",fontWeight:"bolder"}} className={`alert alert-${alertContent.type}`} role="alert">
+            <div style={{position:"absolute",left:"50%",top:"10%",translate:"-50% 0",zIndex:"3",backgroundColor:"lightGreen",color:"white",fontWeight:"bolder"}} className={`alert alert-${alertContent.type}`} role="alert">
                 {alertContent.title}
             </div>
         </>
--- Frontend/src/components/Home/Home.js
+++ Frontend/src/components/Home/Home.js
@@ -1,58 +1,85 @@
-import React,{ useState,useContext } from "react";
+import React, { useState, useContext } from "react";
 import "./Home.scss";
 import ParticlesBg from 'particles-bg'
 import axios from "axios";
 import FileContext from "../../context/FileContext";
 
 
-const Home = ()=>{
+const Home = () => {
 
     const context = useContext(FileContext);
-    const {setAlert} = context;
+    const { setAlert } = context;
 
-    const [data,setData] = useState("");
+    //data is the input file
+    const [data, setData] = useState("");
+    //using formData to store the file
     const formData = new FormData();
 
-    const inputHandler = (e)=>{
+    //upload success handling state
+    const [uploadSuccess, setUploadSuccess] = useState(false);
+    //otp from backend
+    const [otp, setOtp] = useState("");
+
+    const inputHandler = (e) => {
         setData(e.target.files[0]);
     }
 
-    const uploadHandler = async (e)=>{
+    const uploadHandler = async (e) => {
         e.preventDefault();
-        formData.append("data",data);
+        formData.append("data", data);
         try {
-        const response = await axios.post("http://localhost:5000/api/fileUpload",formData,{
-            headers:{
-                "Content-Type":"multipart/form-data"
-            }
-        });
-        setData("");
-        const data = response.data;
-        if(data.success===true){
-            setAlert("success","File Uploaded Successfully")
-        }else{
-            setAlert("danger","Server Error: Try Again")
-        }
-    } catch (error) {
-        console.error(error);
-    }
-    }
-    return(
-    <>
-    
-        <div className="container d-flex justify-content-center">
-            <div className="card d-flex justify-content-center align-items-center  ">
-                    <h1>Upload File</h1>
-                    <form onSubmit={uploadHandler} encType="multipart/form-data">
-                        <input type="file" onChange={inputHandler}/>
-                        {/* adding button disabled state -  */}
-                        <button  type="submit"  disabled={data===""?true:false} className="btn btn-primary">Upload File</button>
-                    </form>
-            </div>
-        </div>
+            const response = await axios.post("http://localhost:5000/api/fileUpload", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            });
+            setData("");
 
-        <ParticlesBg  type="tadpole" bg={true} /> 
-    </>
+            //handling the json response from backend server
+            const data = response.data;
+
+            //checking the response from the server
+            if (data.success === true) {
+                setAlert("success", "File Uploaded Successfully");
+                setUploadSuccess(true);
+                setOtp(data.otp);
+                setTimeout(() => {
+                    setUploadSuccess(false);
+                    setOtp("");
+                }, 15000)
+            } else {
+                setAlert("danger", "Server Error: Try Again");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    return (
+        <>
+
+            <div className="container d-flex justify-content-center">
+                <div className="card d-flex justify-content-center align-items-center  ">
+                    {uploadSuccess ?
+                        <>
+                            <h1>Use Below OTP to Download the File</h1>
+                            <p style={{fontWeight:"bolder",fontSize:"2rem",color:"blue"}}>{otp}</p>
+                            <p style={{fontSize:"1.1rem"}}>OTP is valid for about 5 mins....</p>
+                        </>
+                        :
+                        <>
+                            <h1>Upload File</h1>
+                            <form onSubmit={uploadHandler} encType="multipart/form-data">
+                                <input type="file" onChange={inputHandler} />
+                                {/* adding button disabled state -  */}
+                                <button type="submit" disabled={data === "" ? true : false} className="btn btn-primary">Upload File</button>
+                            </form>
+                        </>
+                    }
+                </div>
+            </div>
+
+            <ParticlesBg type="tadpole" bg={true} />
+        </>
     );
 };
 
